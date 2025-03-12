6318647bb8422cc2162a2fb8006edda0b93d5629
xWhaler <camelbyte@proton.me>
2025-03-12 14:24:29 -0400

    Added a notebook with some metrics I coded like rel volume, moving average, etc.

---
+++ predict.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import os
+from datetime import datetime
+import warnings
+
+
+warnings.filterwarnings("ignore")
+
+
+df = pd.read_csv('data/aapl_stock_prices.csv', delimiter=',')
+
+print(df.sample(5))
+print(df.columns)
+print(df.describe())
+
+
